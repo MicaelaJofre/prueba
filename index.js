32bb40f242ec33bfd7aa438455aa3fc633d8313a
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-21 21:43:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let container = document.querySelector('.diapo');
+let image  = container.querySelector('img')
+
+// Creación de una instancia de Hammer en el contenedor
+const hammer = new Hammer(container);
+console.log(hammer)
+// Habilitar el reconocimiento de gestos de pellizco
+hammer.get('pinch').set({ enable: true });
+
+// Variables para almacenar las transformaciones
+let scaleX = 1;
+let scaleY = 1;
+let lastScaleX = 1;
+let lastScaleY = 1;
+
+// Función para aplicar las transformaciones a la imagen
+function applyTransform() {
+  image.style.transform = `scale(\${scaleX}, \${scaleY})`;
+  console.log(image)
+}
+
+// Escuchar el evento de pellizco y actualizar las escalas
+hammer.on('pinch pinchmove', (event) => {
+  console.log('here')
+  scaleX = lastScaleX * event.scale;
+  scaleY = lastScaleY * event.scale;
+  applyTransform();
+});
+
+// Guardar las escalas al finalizar el pellizco
+hammer.on('pinchend', () => {
+  lastScaleX = scaleX;
+  lastScaleY = scaleY;
+});
